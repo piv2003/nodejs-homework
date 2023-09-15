d85c31191b3340ba379b7c2a4957a837ade38e2b
Ihor Poliakov <piv2003@gmail.com>
2023-09-15 18:53:58 +0200

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,23 @@ import dotenv from "dotenv";
 import contactsRouter from "./routes/api/contacts-router.js";
 
 dotenv.config();
+
 const app = express();
 const formatsLogger = app.get("env") === "development" ? "dev" : "short";
+
+app.use(logger(formatsLogger));
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/contacts", contactsRouter);
+
+app.use((req, res) => {
+  res.status(404).json({ message: "Not found" });
+});
+
+app.use((err, req, res, next) => {
+  const { status = 500, message = "Server error" } = err;
+  res.status(status).json({ message });
+});
 
 export default app;
