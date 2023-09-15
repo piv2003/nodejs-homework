f3aae1c9746a894dc0a7e400a4c37844a5464bae
Ihor Poliakov <piv2003@gmail.com>
2023-09-15 18:52:45 +0200

    Update app.js

--- app.js
+++ app.js
@@ -4,4 +4,8 @@ import cors from "cors";
 import dotenv from "dotenv";
 import contactsRouter from "./routes/api/contacts-router.js";
 
+dotenv.config();
+const app = express();
+const formatsLogger = app.get("env") === "development" ? "dev" : "short";
+
 export default app;
