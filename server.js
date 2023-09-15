161a411451150adf0a52b3f2469e412ff14d1949
Ihor Poliakov <piv2003@gmail.com>
2023-09-15 22:18:10 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,21 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 import app from "./app.js";
 
-app.listen(3001, () => {
-  console.log("Server running. Use our API on port: 3001");
-});
+dotenv.config();
+
+const { DB_HOST, PORT = 3001 } = process.env;
+
+mongoose.set("strictQuery", true);
+
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server running. Use our API on port: ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
