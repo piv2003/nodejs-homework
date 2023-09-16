7e0fe82c738c946a1434b6c1f0ceda552f004e0f
Ihor Poliakov <piv2003@gmail.com>
2023-09-16 14:41:40 +0200

    Update contacts-schemas.js

--- schemas/contacts-schemas.js
+++ schemas/contacts-schemas.js
@@ -1,17 +1,28 @@
 import Joi from "joi";
+import {
+  emailDateRegexp,
+  nameDateRegexp,
+  phoneDateRegexp,
+} from "../constants/contacts-constants.js";
 
 const contactAddSchema = Joi.object({
-  title: Joi.string().required().messages({
-    "any.required": `"title" must be exist`,
+  name: Joi.string().pattern(nameDateRegexp).required().messages({
+    "any.required": `"name" must be exist`,
   }),
-  email: Joi.string().email().required().messages({
+  email: Joi.string().email().pattern(emailDateRegexp).required().messages({
     "any.required": `"email" must be exist`,
   }),
-  phone: Joi.string().required().messages({
+  phone: Joi.string().pattern(phoneDateRegexp).required().messages({
     "any.required": `"phone" must be exist`,
   }),
+  favorite: Joi.boolean().required(),
+});
+
+const updateFavoriteContactSchema = Joi.object({
+  favorite: Joi.boolean().required(),
 });
 
 export default {
   contactAddSchema,
+  updateFavoriteContactSchema,
 };
