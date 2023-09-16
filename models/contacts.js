b6075b1602de7bf4d4c0ad9178aeae72f02bba57
Ihor Poliakov <piv2003@gmail.com>
2023-09-16 14:27:19 +0200

    Update contacts.js

--- models/contacts.js
+++ models/contacts.js
@@ -9,6 +9,31 @@ import {
 
 import { handleSaveError, runValidateAtUpdate } from "./hooks.js";
 
+const contactSchema = new Schema(
+  {
+    name: {
+      type: String,
+      match: nameDateRegexp,
+      required: [true, "Set name for contact"],
+    },
+    email: {
+      type: String,
+      match: emailDateRegexp,
+      required: [true, "Set email for contact"],
+    },
+    phone: {
+      type: String,
+      match: phoneDateRegexp,
+      required: [true, "Set phone for contact"],
+    },
+    favorite: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { versionKey: false, timestamp: true }
+);
+
 const Contact = model("contact", contactSchema);
 
 export { Contact, schemas };
