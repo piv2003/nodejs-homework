880093767a0221731c9c9c7c695112536788c6ae
Ihor Poliakov <piv2003@gmail.com>
2023-09-16 15:20:06 +0200

    Create contacts-constants.js

---
+++ schemas/constants/contacts-constants.js
@@ -0,0 +1,7 @@
+export const nameDateRegexp =
+  /^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$/;
+
+export const emailDateRegexp =
+  /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+export const phoneDateRegexp = /^\(\d{3}\) \d{3}-\d{4}$/;
