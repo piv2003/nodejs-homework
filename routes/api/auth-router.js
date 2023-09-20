c8485284b5540d1efc2f66224457045abfee39b2
Ihor Poliakov <piv2003@gmail.com>
2023-09-20 19:58:58 +0200

    Update auth-router.js

--- routes/api/auth-router.js
+++ routes/api/auth-router.js
@@ -5,5 +5,12 @@ import authController from "../../controllers/auth-controller.js";
 import { checkJwt } from "../../middlewares/index.js";
 
 const authRouter = express.Router();
+const userSignUpValidate = validateBody(userSchemas.userSignUpSchema);
+const userSignInValidate = validateBody(userSchemas.userSignInSchema);
+
+authRouter.post("/register", userSignUpValidate, authController.register);
+authRouter.post("/login", userSignInValidate, authController.login);
+authRouter.get("/logout", checkJwt, authController.logout);
+authRouter.get("/current", checkJwt, authController.current);
 
 export default authRouter;
