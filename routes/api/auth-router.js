d405a5c2df42911582e4edc03cf84c0c2f243ac5
Ihor Poliakov <piv2003@gmail.com>
2023-09-20 19:57:57 +0200

    Update auth-router.js

--- routes/api/auth-router.js
+++ routes/api/auth-router.js
@@ -2,6 +2,7 @@ import express from "express";
 import validateBody from "../../decorators/validateBody.js";
 import userSchemas from "../../schemas/user-schemas.js";
 import authController from "../../controllers/auth-controller.js";
+import { checkJwt } from "../../middlewares/index.js";
 
 const authRouter = express.Router();
 
