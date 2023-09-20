a21b3763dcc5f092558b0fe915440ef5edd821b9
Ihor Poliakov <piv2003@gmail.com>
2023-09-20 19:34:53 +0200

    Update auth-router.js

--- routes/api/auth-router.js
+++ routes/api/auth-router.js
@@ -1,6 +1,7 @@
 import express from "express";
 import validateBody from "../../decorators/validateBody.js";
 import userSchemas from "../../schemas/user-schemas.js";
+import authController from "../../controllers/auth-controller.js";
 
 const authRouter = express.Router();
 
