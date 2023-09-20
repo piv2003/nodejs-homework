7e7771e2f94671a67bfae2aad05c13b036a20002
Ihor Poliakov <piv2003@gmail.com>
2023-09-20 19:34:08 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 import express from "express";
 import logger from "morgan";
 import cors from "cors";
-import dotenv from "dotenv";
+import "dotenv/config";
 import contactsRouter from "./routes/api/contacts-router.js";
+import authRouter from "./routes/api/auth-router.js";
 
 dotenv.config();
 
