423f5ab783c88925576f52733f41b1f3c2b024e5
Ihor Poliakov <piv2003@gmail.com>
2023-09-22 16:08:27 +0200

    Update checkJwt.js

--- middlewares/checkJwt.js
+++ middlewares/checkJwt.js
@@ -10,6 +10,11 @@ const { JWT_SECRET_KEY } = process.env;
 export const checkJwt = async (req, res, next) => {
   const { authorization = "" } = req.headers;
   const [bearer, token] = authorization?.split(" ");
+  try {
+    next();
+  } catch (error) {
+    throw new HttpError(HttpCode.UNAUTHORIZED);
+  }
 };
 
 export default bodyWrapper(checkJwt);
