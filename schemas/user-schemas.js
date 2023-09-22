91467dbbe6c3c1d0fe9b6cef7e46d67724efdbf7
Ihor Poliakov <piv2003@gmail.com>
2023-09-22 16:17:49 +0200

    Update user-schemas.js

--- schemas/user-schemas.js
+++ schemas/user-schemas.js
@@ -1,4 +1,12 @@
 import Joi from "joi";
 import { emailRegexp } from "../constants/user-constants.js";
 
+const userSignUpSchema = Joi.object({
+  email: Joi.string().pattern(emailRegexp).required(),
+  subscription: Joi.string()
+    .valid("starter", "pro", "business")
+    .default("starter"),
+  password: Joi.string().min(6).required(),
+});
+
 export default { userSignUpSchema, userSignInSchema };
