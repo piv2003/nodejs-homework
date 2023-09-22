86e9a94860b134e4a4068064bbe52bc7c8dd2285
Ihor Poliakov <piv2003@gmail.com>
2023-09-22 13:46:59 +0200

    Update isValidId.js

--- middlewares/isValidId.js
+++ middlewares/isValidId.js
@@ -1,10 +1,11 @@
 import { isValidObjectId } from "mongoose";
 import { HttpError } from "../helpers/index.js";
+import { HttpCode } from "../constants/user-constants.js";
 
 const isValidId = (req, res, next) => {
   const { id } = req.params;
   if (!isValidObjectId(id)) {
-    return next(HttpError(404, `${id} is not valid id`));
+    return next(HttpError(HttpCode.NOT_FOUND, `${id} is not valid id`));
   }
   next();
 };
