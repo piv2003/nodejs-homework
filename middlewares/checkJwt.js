739fa3f6b37508d709ca37cbebeae1800eca7f01
Ihor Poliakov <piv2003@gmail.com>
2023-09-22 13:43:02 +0200

    Create checkJwt.js

---
+++ middlewares/checkJwt.js
@@ -0,0 +1,12 @@
+import User from "../models/user.js";
+import jwt from "jsonwebtoken";
+import "dotenv/config.js";
+import HttpError from "../helpers/HTTPError.js";
+import { HttpCode } from "../constants/user-constants.js";
+import bodyWrapper from "../decorators/bodyWrapper.js";
+
+const { JWT_SECRET_KEY } = process.env;
+
+export const checkJwt = async (req, res, next) => {};
+
+export default bodyWrapper(checkJwt);
