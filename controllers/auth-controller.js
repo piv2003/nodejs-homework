ea9f9bc07d6aa118077d984456b427e3c43e40b2
Ihor Poliakov <piv2003@gmail.com>
2023-09-30 21:54:50 +0200

    Update auth-controller.js

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -6,6 +6,9 @@ import fs from "fs/promises";
 import gravatar from "gravatar";
 import Jimp from "jimp";
 import { fileURLToPath } from "url";
+import { HttpCode } from "../constants/user-constants.js";
+import bodyWrapper from "../decorators/bodyWrapper.js";
+import HttpError from "../helpers/HTTPError.js";
 
 const { JWT_SECRET_KEY } = process.env;
 const __filename = fileURLToPath(import.meta.url);
