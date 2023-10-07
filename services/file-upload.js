20bcb445acc929a7925751b7ededeceab06f3dbc
Ihor Poliakov <piv2003@gmail.com>
2023-10-07 14:58:17 +0200

    Update file-upload.js

--- services/file-upload.js
+++ services/file-upload.js
@@ -2,6 +2,10 @@ import Jimp from "jimp";
 import fs from "fs/promises";
 import path from "path";
 
-class UploadFileAvatar {}
+class UploadFileAvatar {
+  constructor(destination) {
+    this.destination = destination;
+  }
+}
 
 export default UploadFileAvatar;
