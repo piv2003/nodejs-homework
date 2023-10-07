059cdc2c6ca09d868f647b338cec985d777e3637
Ihor Poliakov <piv2003@gmail.com>
2023-10-07 14:58:38 +0200

    Update file-upload.js

--- services/file-upload.js
+++ services/file-upload.js
@@ -6,6 +6,19 @@ class UploadFileAvatar {
   constructor(destination) {
     this.destination = destination;
   }
+  async transformAvatar(pathFile) {
+    const pic = Jimp.read(pathFile);
+    await (
+      await pic
+    )
+      .autocrop()
+      .cover(
+        250,
+        250,
+        Jimp.HORIZONTAL_ALIGN_CENTER | Jimp.VERTICAL_ALIGN_MIDDLE
+      )
+      .writeAsync(pathFile);
+  }
 }
 
 export default UploadFileAvatar;
