0c7cf1e7126e738d574b243934ea1ad142657390
Ihor Poliakov <piv2003@gmail.com>
2023-10-08 22:18:57 +0200

    Update contacts.repository.js

--- repository/contacts.repository.js
+++ repository/contacts.repository.js
@@ -1,5 +1,31 @@
 const Contact = require("../model/contactModel");
 
+const removeContact = async (contactId, userId) => {
+  const result = await Contact.findOneAndRemove({
+    _id: contactId,
+    owner: userId,
+  });
+  return result;
+};
+
+const addContact = async (body) => {
+  const result = await Contact.create(body);
+  return result;
+};
+
+const updateContact = async (contactId, body, userId) => {
+  const result = await Contact.findOneAndUpdate(
+    { _id: contactId, owner: userId },
+    { ...body },
+    { new: true }
+  );
+  return result;
+};
 export default {
   Contact,
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+  updateContact,
 };
