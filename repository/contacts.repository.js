31ad9823c5d672e80acfb9cf5b05d151c7fe6649
Ihor Poliakov <piv2003@gmail.com>
2023-10-09 19:43:36 +0200

    Update contacts.repository.js

--- repository/contacts.repository.js
+++ repository/contacts.repository.js
@@ -1,6 +1,35 @@
 const Contact = require("../model/contactModel");
 
-const listContacts = async (userId, query) => {};
+const listContacts = async (userId, query) => {
+  const {
+    sortBy,
+    sortByDesc,
+    filter,
+    favorite = null,
+    limit = 5,
+    offset = 0,
+  } = query;
+  const searchOptions = { owner: userId };
+  if (favorite !== null) {
+    searchOptions.favorite = favorite;
+  }
+  const results = await Contact.paginate(searchOptions, {
+    limit,
+    offset,
+    sort: {
+      ...(sortBy ? { [`${sortBy}`]: 1 } : {}),
+      ...(sortByDesc ? { [`${sortByDesc}`]: 1 } : {}),
+    },
+    select: filter ? filter.split("|").join(" ") : "",
+    populate: {
+      path: "owner",
+      select: "name email gender subscription",
+    },
+  });
+  const { docs: contacts } = results;
+  delete results.docs;
+  return { ...results, contacts };
+};
 const getContactById = async (contactId, userId) => {};
 
 const removeContact = async (contactId, userId) => {
