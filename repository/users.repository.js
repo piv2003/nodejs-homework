ecd86e2fd8edc0daaa5577b513a8a12bc250ed8d
Ihor Poliakov <piv2003@gmail.com>
2023-10-09 19:45:43 +0200

    Update users.repository.js

--- repository/users.repository.js
+++ repository/users.repository.js
@@ -8,7 +8,10 @@ const findByEmail = async (email) => {
   return await User.findOne({ email });
 };
 
-const create = async (options) => {};
+const create = async (options) => {
+  const user = new User(options);
+  return await user.save();
+};
 
 const updateToken = async (id, token) => {};
 
