bbfae5a2ec6fc5333d5ff2b92a096633ec3443f9
Ihor Poliakov <piv2003@gmail.com>
2023-10-09 11:42:19 +0200

    Update sendEmail.js

--- helpers/sendEmail.js
+++ helpers/sendEmail.js
@@ -1,6 +1,8 @@
 import nodemailer from "nodemailer";
 import "dotenv/config";
 
+const { UKR_NET_EMAIL, UKR_NET_PASSWD, PORT, SECURE } = process.env;
+
 const sendEmail = (data) => {
   const email = { ...data, from: UKR_NET_EMAIL };
   return transport.sendMail(email);
